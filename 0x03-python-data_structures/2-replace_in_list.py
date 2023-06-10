909a890add0cc91844416b60360357373640eb34
AmineCoder30 <code30a@gmail.com>
2023-06-10 03:56:16 -0700

    el

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def element_at(my_list, idx, element):
+def replace_in_list(my_list, idx, element):
     if idx < 0:
         return (my_list)
 
@@ -9,3 +9,4 @@ def element_at(my_list, idx, element):
         return (my_list)
     my_list[idx] = element
     return(my_list)
+
