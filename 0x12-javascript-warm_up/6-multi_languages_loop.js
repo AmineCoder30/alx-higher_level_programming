98c499931beea3c4be486a216d8b0a79382ce177
AmineCoder30 <99543252+AmineCoder30@users.noreply.github.com>
2023-09-11 18:19:23 +0200

    Delete 0x12-javascript-warm_up/6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-//script to print some string using loop
-
-const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (let i = 0; i < 3; i++) {
-  console.log(myString[i]);
-}
